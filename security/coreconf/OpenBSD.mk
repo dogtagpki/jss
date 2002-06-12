2a1a1e5c0ac4a559ae7ffad1041b5a99a3643e31
cvs2hg <cvs2hg@localhost>
2002-06-12 00:22:44 +0000

    fixup commit for branch 'BEONEX_0_9_BRANCH'

--- security/coreconf/OpenBSD.mk
+++ security/coreconf/OpenBSD.mk
@@ -1,27 +1,27 @@
-# 
+#
 # The contents of this file are subject to the Mozilla Public
 # License Version 1.1 (the "License"); you may not use this file
 # except in compliance with the License. You may obtain a copy of
 # the License at http://www.mozilla.org/MPL/
-# 
+#
 # Software distributed under the License is distributed on an "AS
 # IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
 # implied. See the License for the specific language governing
 # rights and limitations under the License.
-# 
-# The Original Code is the Netscape Security Services for Java.
-# 
+#
+# The Original Code is the Netscape security libraries.
+#
 # The Initial Developer of the Original Code is Netscape
-# Communications Corporation.  Portions created by Netscape are 
-# Copyright (C) 1998-2000 Netscape Communications Corporation.  All
+# Communications Corporation.  Portions created by Netscape are
+# Copyright (C) 1994-2000 Netscape Communications Corporation.  All
 # Rights Reserved.
-# 
+#
 # Contributor(s):
-# 
+#
 # Alternatively, the contents of this file may be used under the
 # terms of the GNU General Public License Version 2 or later (the
-# "GPL"), in which case the provisions of the GPL are applicable 
-# instead of those above.  If you wish to allow use of your 
+# "GPL"), in which case the provisions of the GPL are applicable
+# instead of those above.  If you wish to allow use of your
 # version of this file only under the terms of the GPL and not to
 # allow others to use your version of this file under the MPL,
 # indicate your decision by deleting the provisions above and
@@ -29,38 +29,34 @@
 # the GPL.  If you do not delete the provisions above, a recipient
 # may use your version of this file under either the MPL or the
 # GPL.
-# 
+#
+# Config stuff for OpenBSD 
+#
 
-CORE_DEPTH = ../../../../../..
+include $(CORE_DEPTH)/coreconf/UNIX.mk
 
-MODULE = jss
+DEFAULT_COMPILER	= gcc
+CC			= gcc
+CCC			= g++
+RANLIB			= ranlib
 
-NS_USE_JDK = 1
+CPU_ARCH		:= $(shell uname -p)
+ifeq ($(CPU_ARCH),i386)
+OS_REL_CFLAGS		= -Di386
+CPU_ARCH		= x86
+endif
 
+DLL_SUFFIX		= so.1.0
 
-PACKAGE =       org/mozilla/jss/pkix/cmc
+OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -ansi -Wall -pipe -DOPENBSD
 
+OS_LIBS			= 
 
-CLASSES =	            \
-            PKIData \
-            TaggedCertificationRequest \
-            TaggedRequest \
-            ResponseBody \
-            TaggedAttribute \
-            CMCStatusInfo \
-            PendInfo \
-            OtherInfo \
-            $(NULL)
+ARCH			= openbsd
 
+DSO_CFLAGS		= -fPIC -DPIC
+DSO_LDOPTS		= -shared -Wl,-soname,lib$(LIBRARY_NAME)$(LIBRARY_VERSION).$(DLL_SUFFIX)
+DSO_LDFLAGS		=
 
+MKSHLIB			= $(CC) $(DSO_LDOPTS)
 
-JSRCS =	                       \
-            PKIData.java \
-            TaggedCertificationRequest.java \
-            TaggedRequest.java \
-            ResponseBody.java \
-            TaggedAttribute.java \
-            CMCStatusInfo.java \
-            PendInfo.java \
-            OtherInfo.java \
-            $(NULL)
