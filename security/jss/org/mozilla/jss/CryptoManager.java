5ff85acaac127261ce7a2c29baa8c84d8008e13e
wchang0222%aol.com <wchang0222%aol.com@localhost>
2003-11-13 23:39:46 +0000

    Upgraded to NSS_3_7_9_RTM (from NSS_3_7_8_RTM). Modified Files: manifest.mn org/mozilla/jss/CryptoManager.java

--- security/jss/org/mozilla/jss/CryptoManager.java
+++ security/jss/org/mozilla/jss/CryptoManager.java
@@ -1313,7 +1313,7 @@ public final class CryptoManager implements TokenSupplier
     public static final String
     JAR_JDK_VERSION     = "JDK_VERSION = N/A";
     public static final String
-    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_7_8_RTM";
+    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_7_9_RTM";
     public static final String
     JAR_DBM_VERSION     = "DBM_VERSION = N/A";
     public static final String
