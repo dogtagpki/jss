1493378936721d7531f9883d657d1c6d906d8fae
glen.beasley%sun.com <glen.beasley%sun.com@localhost>
2004-10-21 23:56:46 +0000

    220223 TokenException caused by left over cert8.db

--- security/jss/org/mozilla/jss/tests/all.pl
+++ security/jss/org/mozilla/jss/tests/all.pl
@@ -107,7 +107,7 @@ sub setup_vars {
     $java = $java . $ENV{NATIVE_FLAG};
 
     if ($ENV{USE_64}) {
-	$java = $java . " -d64";
+        $java = $java . " -d64";
     }
 
     $pwfile = "passwords";
@@ -137,9 +137,15 @@ if( ! -d $testdir ) {
 {
     chdir "testdir" or die;
     my @dbfiles = 
-        ("./cert7.db", "./key3.db", "./secmod.db", "./secmodule.db");
+        ("./cert8.db", "./key3.db", "./secmod.db");
     unlink @dbfiles;
     (grep{ -f } @dbfiles)  and die "Unable to delete old database files";
+    # if dbdir exists delete it
+    my $dbdir = "./cert8.dir";
+    if (-d $dbdir ) {
+      unlink(< $dbdir/* >);     # delete all files in cert8.dir
+      rmdir($dbdir) or die "Unable to delete cert8.dir";
+    }    
     chdir ".." or die;
     my $result = system("cp $nss_lib_dir/*nssckbi* testdir"); $result >>= 8;
     $result and die "Failed to copy builtins library";
