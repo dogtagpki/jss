ca2e0f0adfb1d4af9517e08abd02f37325c286eb
sandeep.konchady%sun.com <sandeep.konchady%sun.com@localhost>
2005-08-18 19:58:52 +0000

    Updating JSS revision to 3.1.9.

--- security/jss/org/mozilla/jss/manage/CryptoManager.c
+++ security/jss/org/mozilla/jss/manage/CryptoManager.c
@@ -67,7 +67,7 @@
 /*                                                                  */
 /********************************************************************/
 
-static const char* DLL_JSS_VERSION     = "JSS_VERSION = JSS_3_1_8";
+static const char* DLL_JSS_VERSION     = "JSS_VERSION = JSS_3_1_9";
 static const char* DLL_JDK_VERSION     = "JDK_VERSION = JDK 1.4.0";
 static const char* DLL_NSS_VERSION     = "NSS_VERSION = NSS_3_3_11";
 static const char* DLL_DBM_VERSION     = "DBM_VERSION = NSS_3_3_11";
