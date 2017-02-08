ec18834f83f55177421d0b921ca9ce4e6ddf1347
Elio Maldonado <emaldona@redhat.com>
2017-02-08 15:44:09 -0800

    Bug 1308017 - Un-deprecate previously deprecated methods in JSS 4.2.6

--- org/mozilla/jss/crypto/JSSSecureRandom.java
+++ org/mozilla/jss/crypto/JSSSecureRandom.java
@@ -6,7 +6,6 @@ package org.mozilla.jss.crypto;
 
 /**
  * An interface for secure random numbers.
- * @deprecated Use the JCA interface instead ({@link java.security.SecureRandom})
  */
 public interface JSSSecureRandom {
 
