a1f1396aeb425474bd76372c7e66c00064bd315f
Fraser Tweedale <ftweedal@redhat.com>
2018-08-07 03:28:59 +1000

    Revert "Release 4.5.1"
    This reverts commit 4c740a594767ffdd94b807d7b3f653f79434e427.
    
    4.5.0 was not released yet, so revert the version bump to 4.5.1 and
    put the new symbol under the 4.5 shared library version.

--- org/mozilla/jss/util/jssver.h
+++ org/mozilla/jss/util/jssver.h
@@ -25,10 +25,10 @@
 /*                                                                  */
 /********************************************************************/
 
-#define JSS_VERSION  "4.5.1"
+#define JSS_VERSION  "4.5.0"
 #define JSS_VMAJOR   4
 #define JSS_VMINOR   5
-#define JSS_VPATCH   1
+#define JSS_VPATCH   0
 #define JSS_BETA     PR_FALSE
 
 #endif
