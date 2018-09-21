4be224c70bf2d7d79db7700ed4352b4aceed90b6
Alexander Scheel <ascheel@redhat.com>
2018-09-21 10:16:25 -0400

    Add code style checking script for Perl code
    This introduces a new directory into the repository, tools/ which will
    contain utilities not critical to the functioning of JSS but which are
    helpful to the development of JSS.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ tools/test_perl_style.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#
+# Usage:
+# test_perl_style.sh [path]
+#
+# Tests the style of Perl scripts in the build tree for compliance with strict
+# guidance. Runs:
+#
+#     perl -Mstrict -Mdiagnostics -cw <file>
+#
+# On all checked files. If [path] is specified, assumed to be the root of the
+# jss repository. Otherwise, defaults to `pwd`.
+#
+
+root_source_dir="$1"
+if [ "x$root_source_dir" = "x" ]; then
+    root_source_dir="$(pwd)"
+fi
+
+perl_check() {
+    target_file="$1"
+
+    perl -Mstrict -Mdiagnostics -cw "$root_source_dir/$target_file"
+}
+
+
+perl_check "build_java.pl"
