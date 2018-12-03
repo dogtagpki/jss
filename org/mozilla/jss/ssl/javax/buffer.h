5f5019064ed4d3e4ff80d78038e9236f7fc1fa24
Alexander Scheel <ascheel@redhat.com>
2018-12-03 16:08:04 -0500

    Add ring buffer implementation
    This buffer will be used as the in-memory buffer for SSLEngine via
    BufferPRFD. This provides a common interface to reading/writing to a
    fixed location in memory.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ org/mozilla/jss/ssl/javax/buffer.h
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <string.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma once
+
+typedef struct {
+    uint8_t *contents;
+    size_t capacity;
+
+    size_t write_pos;
+    size_t read_pos;
+} j_buffer;
+
+j_buffer *jb_alloc(size_t length);
+
+size_t jb_capacity(j_buffer *buf);
+bool jb_can_read(j_buffer *buf);
+bool jb_can_write(j_buffer *buf);
+
+int jb_put(j_buffer *buf, uint8_t byte);
+size_t jb_write(j_buffer *buf, uint8_t *input, size_t input_size);
+
+int jb_get(j_buffer *buf);
+size_t jb_read(j_buffer *buf, uint8_t *output, size_t output_size);
+
+void jb_free(j_buffer *buf);
