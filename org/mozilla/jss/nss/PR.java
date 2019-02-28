075c903f21c4e8394ce00e263824a7d4626cdefe
Alexander Scheel <ascheel@redhat.com>
2019-02-28 10:25:50 -0500

    Add common PRFileDesc functions
    Several NSPR methods operate on PRFileDesc objects which are useful to
    access from Java:
    
     - Read/Write, Send/Recv -- for IO operations on NSS SSL sockets
     - NewTCPSocket -- to hand off a socket to be wrapped by NSS
     - Shutdown -- to close a network socket
     - Open -- for testing purposes to create files
    
    This adds these methods and their implementations in Java.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ org/mozilla/jss/nss/PR.java
@@ -0,0 +1,31 @@
+package org.mozilla.jss.nss;
+
+/**
+ * This class provides static access to raw NSPS calls with the PR prefix,
+ * and handles the usage of NativeProxy objects.
+ */
+
+public class PR {
+
+    public static native PRFDProxy Open(String name, int flags, int mode);
+
+    public static native PRFDProxy NewTCPSocket();
+
+    public static native int Close(PRFDProxy fd);
+
+    public static native int Shutdown(PRFDProxy fd, int how);
+
+    public static native byte[] Read(PRFDProxy fd, int amount);
+
+    public static native byte[] Recv(PRFDProxy fd, int amount, int flags,
+                                     long timeout);
+
+    public static native int Write(PRFDProxy fd, byte[] buf);
+
+    public static native int Send(PRFDProxy fd, byte[] buf, int flags,
+                                  long timeout);
+
+    public static native int GetError();
+
+    public static native byte[] GetErrorText();
+}
