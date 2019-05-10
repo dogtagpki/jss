306aed92364b02202a07f04f4b5b545f1bfbef93
Alexander Scheel <ascheel@redhat.com>
2019-05-10 13:30:52 -0400

    Declare certList earlier in ssl/common.c:getRoot
    This fixes the following compilation warning:
    
    jss/org/mozilla/jss/ssl/common.c: In function ‘getRoot’:
    jss/org/mozilla/jss/ssl/common.c:923:5: warning: ‘certList’ may be used uninitialized in this function [-Wmaybe-uninitialized]
         CERT_DestroyCertList (certList);
         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/ssl/common.c
+++ org/mozilla/jss/ssl/common.c
@@ -893,21 +893,20 @@ finish:
 
 /* Get the trusted anchor for pkix */
 
-CERTCertificate * getRoot(CERTCertificate *cert, 
+CERTCertificate *getRoot(CERTCertificate *cert,
     SECCertUsage certUsage) 
 {
     CERTCertificate  *root = NULL;
     CERTCertListNode *node = NULL;
+    CERTCertList *certList = NULL;
 
-    if( !cert ) {
+    if (!cert) {
         goto finish;
     }
 
-    CERTCertList *certList =  CERT_GetCertChainFromCert(cert, 
-        PR_Now(), 
-        certUsage);
+    certList = CERT_GetCertChainFromCert(cert, PR_Now(), certUsage);
 
-    if( certList == NULL) {
+    if (certList == NULL) {
         goto finish;
     }
 
@@ -923,7 +922,7 @@ CERTCertificate * getRoot(CERTCertificate *cert,
 
 finish:
   
-    CERT_DestroyCertList (certList); 
+    CERT_DestroyCertList(certList);
     return root; 
 }
 
