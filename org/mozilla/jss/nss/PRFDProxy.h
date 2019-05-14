e568038fb4c74e63b2de275854bd5bfae5e3d1eb
Alexander Scheel <ascheel@redhat.com>
2019-05-14 15:38:47 -0400

    Add documentation to nss/PRFDProxy.h
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/PRFDProxy.h
+++ org/mozilla/jss/nss/PRFDProxy.h
@@ -3,5 +3,8 @@
 
 #pragma once
 
+/* Wrap a C/NSPR PRFileDesc into a Java PRFDProxy, closing the fd on error. */
 jobject JSS_PR_wrapPRFDProxy(JNIEnv *env, PRFileDesc **fd);
+
+/* Extract the C/NSPR PRFileDesc from an instance of a Java PRFDProxy. */
 PRStatus JSS_PR_getPRFileDesc(JNIEnv *env, jobject prfd_proxy, PRFileDesc **fd);
