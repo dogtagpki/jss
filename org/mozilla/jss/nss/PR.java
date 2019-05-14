13b276f147071ed210931f0adec902106699f3d7
Alexander Scheel <ascheel@redhat.com>
2019-05-14 15:33:40 -0400

    Add documentation to jss.nss.PR
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/PR.java
+++ org/mozilla/jss/nss/PR.java
@@ -7,25 +7,84 @@ package org.mozilla.jss.nss;
 
 public class PR {
 
+    /**
+     * Open the file at name (with the specified flags and mode) and create
+     * a new PRFDProxy (to a NSPR PRFileDesc *) for that file.
+     *
+     * See also: PR_Open in /usr/include/nspr4/prio.h
+     */
     public static native PRFDProxy Open(String name, int flags, int mode);
 
+    /**
+     * Open a new TCP Socket and create a new PRFDProxy for that socket.
+     *
+     * See also: PR_NewTCPSocket in /usr/include/nspr4/prio.h
+     */
     public static native PRFDProxy NewTCPSocket();
 
+    /**
+     * Close an existing PRFDProxy.
+     *
+     * See also: PR_Close in /usr/include/nspr4/prio.h
+     */
     public static native int Close(PRFDProxy fd);
 
+    /**
+     * Shutdown an existing PRFDProxy.
+     * This is usually used with TCP modes.
+     *
+     * See also: PR_Shutdown in /usr/include/nspr4/prio.h
+     */
     public static native int Shutdown(PRFDProxy fd, int how);
 
+    /**
+     * Read up to amount bytes from a PRFDProxy.
+     *
+     * See also: PR_Read in /usr/include/nspr4/prio.h
+     */
     public static native byte[] Read(PRFDProxy fd, int amount);
 
+    /**
+     * Recv up to amount bytes from a PRFDProxy, given the specified receive
+     * flags and timeout value.
+     *
+     * See also: PR_Recv in /usr/include/nspr4/prio.h
+     */
     public static native byte[] Recv(PRFDProxy fd, int amount, int flags,
                                      long timeout);
 
+    /**
+     * Write the specified bytes to the PRFDProxy.
+     *
+     * Note: Unlike PR_Write, this method assumes the entire buffer is being
+     * written.
+     *
+     * See also: PR_Write in /usr/include/nspr4/prio.h
+     */
     public static native int Write(PRFDProxy fd, byte[] buf);
 
+    /**
+     * Send the specified bytes via the PRFDProxy, given the specified
+     * send flags and timeout value.
+     *
+     * See also: PR_Send in /usr/include/nspr4/prio.h
+     */
     public static native int Send(PRFDProxy fd, byte[] buf, int flags,
                                   long timeout);
 
+    /**
+     * Get the value of the current PR error. This is cleared on each NSPR
+     * call.
+     *
+     * See also: PR_GetError in /usr/include/nspr4/prio.h
+     */
     public static native int GetError();
 
+    /**
+     * Get the error text of the current PR error. This is cleared on each
+     * NSPR call.
+     *
+     * See also: PR_GetErrorText in /usr/include/nspr4/prio.h
+     */
     public static native byte[] GetErrorText();
 }
