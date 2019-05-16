7c9e2001e24e45e869b0c1de4d1a2d065c2d5181
Alexander Scheel <ascheel@redhat.com>
2019-05-16 14:54:55 -0400

    Document jss.nss.SecurityStatusResult
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/SecurityStatusResult.java
+++ org/mozilla/jss/nss/SecurityStatusResult.java
@@ -1,11 +1,29 @@
 package org.mozilla.jss.nss;
 
+/**
+ * The fields in a SecurityStatusResult indicate whether a given SSL-enabled
+ * PRFileDesc has completed its handshake and the resulting handshake-specific
+ * information.
+ *
+ * These object is returned by org.mozilla.jss.nss.SSL.SecurityStatus(fd).
+ */
 public class SecurityStatusResult {
+    /* Whether or not the handshake has completed successfully. */
     public int on;
+
+    /* The current ciphersuite used. */
     public byte[] cipher;
+
+    /* Size of the negotiated peer key. */
     public int keySize;
+
+    /* Size of the session secret key. */
     public int secretKeySize;
+
+    /* Issuer of the peer's certificate. */
     public byte[] issuer;
+
+    /* Subject of the peer's certificate. */
     public byte[] subject;
 
     public SecurityStatusResult(int _on, byte[] _cipher, int _keySize,
