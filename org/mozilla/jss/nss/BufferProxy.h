67caf2d75b37850ff8ac80b2866c49473576fe6f
Alexander Scheel <ascheel@redhat.com>
2019-05-16 15:03:21 -0400

    Document BufferProxy native methods
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/BufferProxy.h
+++ org/mozilla/jss/nss/BufferProxy.h
@@ -3,5 +3,8 @@
 
 #pragma once
 
+/* Wrap a j_buffer object into a BufferProxy, freeing the buffer on error. */
 jobject JSS_PR_wrapJBuffer(JNIEnv *env, j_buffer **buffer);
+
+/* Extract a j_buffer pointer from an instance of a BufferProxy. */
 PRStatus JSS_PR_unwrapJBuffer(JNIEnv *env, jobject buffer_proxy, j_buffer **buffer);
