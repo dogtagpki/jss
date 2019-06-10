07d3175e41cc78b58566e0ad584851bc268a6f48
Alexander Scheel <ascheel@redhat.com>
2019-06-10 17:30:03 -0400

    Use new SECStatus constants in test cases
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/tests/TestRawSSL.java
+++ org/mozilla/jss/tests/TestRawSSL.java
@@ -15,7 +15,7 @@ public class TestRawSSL {
         PRFDProxy ssl_fd = SSL.ImportFD(null, fd);
         assert(ssl_fd != null);
 
-        assert(PR.Close(ssl_fd) == 0);
+        assert(PR.Close(ssl_fd) == SSL.SECSuccess);
     }
 
     public static void TestSSLOptions() throws Exception {
@@ -26,17 +26,17 @@ public class TestRawSSL {
         assert(ssl_fd != null);
 
         // 8 == SSL_ENABLE_SSL3; disable it
-        assert(SSL.OptionSet(ssl_fd, 8, 0) == 0);
+        assert(SSL.OptionSet(ssl_fd, 8, 0) == SSL.SECSuccess);
 
         // Validate that the set worked.
-        assert(SSL.OptionGet(ssl_fd, 8) == 0);
+        assert(SSL.OptionGet(ssl_fd, 8) == SSL.SECSuccess);
 
         // Renable SSL_ENABLE_SSL3 and validate it worked
-        assert(SSL.OptionSet(ssl_fd, 8, 1) == 0);
+        assert(SSL.OptionSet(ssl_fd, 8, 1) == SSL.SECSuccess);
         assert(SSL.OptionGet(ssl_fd, 8) == 1);
 
         // Ensure that setting an invalid option fails
-        assert(SSL.OptionSet(ssl_fd, 799999, 0) != 0);
+        assert(SSL.OptionSet(ssl_fd, 799999, 0) != SSL.SECSuccess);
 
         // Ensure that getting an invalid option fails
         try {
@@ -59,14 +59,14 @@ public class TestRawSSL {
         int cipher = SSLCipher.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384.getID();
 
         // Ensure that setting a ciphersuite works correctly
-        assert(SSL.CipherPrefSet(ssl_fd, cipher, false) == 0);
+        assert(SSL.CipherPrefSet(ssl_fd, cipher, false) == SSL.SECSuccess);
         assert(SSL.CipherPrefGet(ssl_fd, cipher) == false);
 
-        assert(SSL.CipherPrefSet(ssl_fd, cipher, true) == 0);
+        assert(SSL.CipherPrefSet(ssl_fd, cipher, true) == SSL.SECSuccess);
         assert(SSL.CipherPrefGet(ssl_fd, cipher) == true);
 
         // Ensure that using an invalid ciphersuite fails.
-        assert(SSL.CipherPrefSet(ssl_fd, 0x999999, false) != 0);
+        assert(SSL.CipherPrefSet(ssl_fd, 0x999999, false) == SSL.SECFailure);
         try {
             SSL.CipherPrefGet(ssl_fd, 0x999999);
             assert(false);
@@ -81,12 +81,12 @@ public class TestRawSSL {
         PRFDProxy fd = PR.NewTCPSocket();
         assert(fd != null);
 
-        assert(SSL.SetURL(fd, "https://google.com") != 0);
+        assert(SSL.SetURL(fd, "https://google.com") == SSL.SECFailure);
 
         PRFDProxy ssl_fd = SSL.ImportFD(null, fd);
         assert(ssl_fd != null);
 
-        assert(SSL.SetURL(ssl_fd, "https://google.com") == 0);
+        assert(SSL.SetURL(ssl_fd, "https://google.com") == SSL.SECSuccess);
 
         assert(PR.Close(ssl_fd) == 0);
     }
@@ -110,10 +110,10 @@ public class TestRawSSL {
         PRFDProxy fd = PR.NewTCPSocket();
         assert(fd != null);
 
-        assert(SSL.ResetHandshake(fd, false) != 0);
+        assert(SSL.ResetHandshake(fd, false) == SSL.SECFailure);
 
         PRFDProxy ssl_fd = SSL.ImportFD(null, fd);
-        assert(SSL.ResetHandshake(fd, false) == 0);
+        assert(SSL.ResetHandshake(fd, false) == SSL.SECSuccess);
 
         assert(PR.Close(ssl_fd) == 0);
     }
