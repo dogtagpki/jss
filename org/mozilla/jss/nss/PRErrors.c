499e9080d82a928891573950ce024afda3c51898
Alexander Scheel <ascheel@redhat.com>
2019-06-10 20:39:25 -0400

    Add PR Error constants
    Some org.mozilla.jss.nss.PR methods return errors as ints; add
    org.mozilla.jss.nss.PRErrors which contains constants for common errors.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ org/mozilla/jss/nss/PRErrors.c
@@ -0,0 +1,17 @@
+#include <nspr.h>
+#include <limits.h>
+#include <stdint.h>
+#include <jni.h>
+
+#include "jssutil.h"
+#include "PRFDProxy.h"
+#include "BufferProxy.h"
+#include "BufferPRFD.h"
+
+#include "_jni/org_mozilla_jss_nss_PRErrors.h"
+
+JNIEXPORT int JNICALL
+Java_org_mozilla_jss_nss_PRErrors_getWouldBlockError(JNIEnv *env, jclass clazz)
+{
+    return PR_WOULD_BLOCK_ERROR;
+}
