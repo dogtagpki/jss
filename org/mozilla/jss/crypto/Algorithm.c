68bd9a7064292dc08d4ae62fdbf00617644b31d5
Alexander Scheel <ascheel@redhat.com>
2019-07-22 14:28:57 -0400

    Expose SHA-2 HMAC in SymmetricKey
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/crypto/Algorithm.c
+++ org/mozilla/jss/crypto/Algorithm.c
@@ -94,8 +94,11 @@ JSS_AlgInfo JSS_AlgTable[NUM_ALGS] = {
 /* 64 */    {SEC_OID_AES_256_CBC, SEC_OID_TAG},
 /* the CKM_AES_KEY_WRAP_* have different defs than CKM_NSS_AES_KEY_WRAP_*  */
 /* 65 */    {CKM_AES_KEY_WRAP, PK11_MECH},
-/* 66 */    {CKM_AES_KEY_WRAP_PAD, PK11_MECH}
-/* REMEMBER TO UPDATE NUM_ALGS!!! */
+/* 66 */    {CKM_AES_KEY_WRAP_PAD, PK11_MECH},
+/* 67 */    {CKM_SHA256_HMAC, PK11_MECH},
+/* 68 */    {CKM_SHA384_HMAC, PK11_MECH},
+/* 69 */    {CKM_SHA512_HMAC, PK11_MECH}
+/* REMEMBER TO UPDATE NUM_ALGS!!! (in Algorithm.h) */
 };
 
 /***********************************************************************
--- org/mozilla/jss/crypto/Algorithm.h
+++ org/mozilla/jss/crypto/Algorithm.h
@@ -24,7 +24,7 @@ typedef struct JSS_AlgInfoStr {
     JSS_AlgType type;
 } JSS_AlgInfo;
 
-#define NUM_ALGS 67
+#define NUM_ALGS 70
 
 extern JSS_AlgInfo JSS_AlgTable[];
 extern CK_ULONG JSS_symkeyUsage[];
