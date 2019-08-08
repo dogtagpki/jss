41d1e64b6409865ff51d887845e42b1bda5e6120
Alexander Scheel <ascheel@redhat.com>
2019-08-08 11:36:43 -0400

    Implement ECPublicKey with PK11ECPublicKey
    With the new curve database and the NSS helpers, we can now implement
    the standard ECPublicKey interface from our PK11ECPublicKey class,
    bringing JSS into compliance.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/pkcs11/PK11ECPublicKey.java
+++ org/mozilla/jss/pkcs11/PK11ECPublicKey.java
@@ -4,51 +4,35 @@
 
 package org.mozilla.jss.pkcs11;
 
-// Requires JAVA 1.5
-//import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECPoint;
+import java.security.spec.ECParameterSpec;
 import java.math.BigInteger;
 
-//
-// Requires JAVA 1.5
-//public final class PK11ECPublicKey extends PK11PubKey implements ECPublicKey {
-public final class PK11ECPublicKey extends PK11PubKey {
+import org.mozilla.jss.netscape.security.util.Utils;
+import org.mozilla.jss.util.EC;
+
+public final class PK11ECPublicKey extends PK11PubKey implements ECPublicKey {
 
     private static final long serialVersionUID = 1L;
     public PK11ECPublicKey(byte[] pointer) {
         super(pointer);
     }
 
-//
-// Requires JAVA 1.5
-//    public ECParams getCurve() {
-//      try {
-//        return new BigInteger( getCurveByteArray() );
-//      } catch(NumberFormatException e) {
-//          throw new RuntimeException("Unable to decode DSA parameters: " + e.getMessage(), e);
-//      }
-//    }
-//
-
-    public BigInteger getCurve() {
-      try {
-        return new BigInteger( getCurveByteArray() );
-      } catch(NumberFormatException e) {
-          throw new RuntimeException("Unable to decode EC curve: " + e.getMessage(), e);
-      }
+    public ECParameterSpec getParams() {
+        byte[] curveData = getCurveByteArray();
+        return EC.decodeNSSOID(curveData);
     }
 
     public byte[] getCurveBA() {
         return getCurveByteArray();
     }
 
-    public BigInteger getW() {
-      try {
-        return new BigInteger( getWByteArray() );
-      } catch(NumberFormatException e) {
-          throw new RuntimeException("Unable to decode EC public value: " + e.getMessage(), e);
-      }
+    public ECPoint getW() {
+        byte[] pointData = getWByteArray();
+        return EC.decodeNSSPoint(pointData);
     }
 
     private native byte[] getCurveByteArray();
-    private native byte[] getWByteArray();
+    public native byte[] getWByteArray();
 }
