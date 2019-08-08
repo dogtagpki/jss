d722d809595ebe13625aedc1e588693061aef54c
Alexander Scheel <ascheel@redhat.com>
2019-08-08 11:36:33 -0400

    Re-add X509CertTest
    This test exercises the ECPublicKey and ECPrivateKey interfaces that are
    newly added to the public/private key classes in org.mozilla.jss.pkcs11.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/tests/X509CertTest.java
+++ org/mozilla/jss/tests/X509CertTest.java
@@ -9,8 +9,11 @@ import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.PrivateKey;
 import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPublicKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.ECPrivateKey;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -34,77 +37,138 @@ import org.mozilla.jss.netscape.security.x509.X509CertImpl;
 import org.mozilla.jss.netscape.security.x509.X509CertInfo;
 import org.mozilla.jss.netscape.security.x509.X509Key;
 import org.mozilla.jss.pkcs11.PK11ECPublicKey;
+import org.mozilla.jss.util.PasswordCallback;
 
 public class X509CertTest {
 
     public static String subjectDN = "CN = 8a99f98342b97d130142ba2cc30f07d3";
-    public static String issuerDN= "C = US, ST = North Carolina, O = Red Hat Inc., OU = Red Hat Network, CN = Red Hat Candlepin Authority, E = ca-support@redhat.com ";
+    public static String issuerDN = "C = US, ST = North Carolina, O = Red Hat Inc., OU = Red Hat Network, CN = Red Hat Candlepin Authority, E = ca-support@redhat.com ";
 
     /* Just some sample code to exercise the new classes */
-    public static void main(String []args) {
+    public static void main(String []args) throws Exception {
+        if( args.length != 2 ) {
+            System.out.println("Usage: X509CertTest  <dbdir> <passwordfile>");
+            return;
+        }
 
+        String dbdir = args[0];
+        String passwordfile = args[1];
+
+
+        Date notBefore = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(notBefore);
+        cal.set(Calendar.YEAR, 2037);
+
+        Date notAfter = cal.getTime();
+
+        //Generate ca keyPair
+        CryptoManager.initialize(dbdir);
+        CryptoManager cryptoManager = CryptoManager.getInstance();
+        CryptoToken token = cryptoManager.getInternalKeyStorageToken();
+        PasswordCallback cb = new FilePasswordCallback(passwordfile);
+        token.login(cb);
+
+        testEC(token, notBefore, notAfter);
+        testRSA(token, notBefore, notAfter);
+    }
+
+    public static void testEC(CryptoToken token, Date notBefore, Date notAfter) throws Exception {
         X509CertImpl certImpl = null;
         X509CertInfo certInfo = null;
 
-        if( args.length != 1 ) {
-                System.out.println("Usage: X509CertTest  <dbdir>");
-                return;
-            }
-            String dbdir = args[0];
-
-        try {
-
-            Date notBefore = new Date();
-            Calendar cal=Calendar.getInstance();
-            cal.setTime(notBefore);
-            cal.set(Calendar.YEAR,2037);
-
-            Date notAfter = cal.getTime();
-
-            //Generate ca keyPair
-
-            CryptoManager.initialize(dbdir);
-            CryptoManager cryptoManager = CryptoManager.getInstance();
-            CryptoToken token = cryptoManager.getInternalKeyStorageToken();
-            KeyPairGenerator gen = token.getKeyPairGenerator(KeyPairAlgorithm.RSA);
-            gen.initialize(4096);
-            KeyPair keypairCA = gen.genKeyPair();
-            PublicKey pubCA = keypairCA.getPublic();
+        KeyPairGenerator gen = token.getKeyPairGenerator(KeyPairAlgorithm.EC);
+        gen.initialize(gen.getCurveCodeByName("secp521r1"));
+        KeyPair keypairCA = gen.genKeyPair();
+        testKeys(keypairCA);
+        PublicKey pubCA = keypairCA.getPublic();
 
 
-            gen.initialize(4096);
-            KeyPair keypairUser = gen.genKeyPair();
-            PublicKey pubUser = keypairUser.getPublic();
+        gen.initialize(gen.getCurveCodeByName("secp521r1"));
+        KeyPair keypairUser = gen.genKeyPair();
+        testKeys(keypairUser);
+        PublicKey pubUser = keypairUser.getPublic();
 
-            CertificateIssuerName issuernameObj =
-                    new CertificateIssuerName(new X500Name(issuerDN));
+        CertificateIssuerName issuernameObj = new CertificateIssuerName(new X500Name(issuerDN));
 
-            certInfo = createX509CertInfo(convertPublicKeyToX509Key(pubUser),
-                BigInteger.valueOf(1),issuernameObj,subjectDN,
-                notBefore, notAfter, "SHA256withRSA");
+        certInfo = createX509CertInfo(convertPublicKeyToX509Key(pubUser),
+                                      BigInteger.valueOf(1),issuernameObj,subjectDN,
+                                      notBefore, notAfter, "SHA256withEC");
 
-           certImpl = new X509CertImpl(certInfo);
-           certImpl.sign(keypairCA.getPrivate(),"SHA256withRSA");
+        certImpl = new X509CertImpl(certInfo);
+        certImpl.sign(keypairCA.getPrivate(),"SHA256withEC");
 
-           String certOutput = certImpl.toString();
+        String certOutput = certImpl.toString();
 
-           System.out.println("Test certificate output: \n" + certOutput);
-
-
-        } catch( Exception e ) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.exit(0);
+        System.out.println("Test certificate output: \n" + certOutput);
     }
 
-    public static X509CertInfo createX509CertInfo(X509Key x509key,
-            BigInteger serialno, CertificateIssuerName issuernameObj, String subjname,
-            Date notBefore, Date notAfter, String alg)
-            throws IOException,
-            CertificateException,
-            InvalidKeyException,
-            NoSuchAlgorithmException {
+    public static void testRSA(CryptoToken token, Date notBefore, Date notAfter) throws Exception {
+        X509CertImpl certImpl = null;
+        X509CertInfo certInfo = null;
+
+        KeyPairGenerator gen = token.getKeyPairGenerator(KeyPairAlgorithm.RSA);
+        gen.initialize(4096);
+        KeyPair keypairCA = gen.genKeyPair();
+        testKeys(keypairCA);
+        PublicKey pubCA = keypairCA.getPublic();
+
+
+        gen.initialize(4096);
+        KeyPair keypairUser = gen.genKeyPair();
+        testKeys(keypairUser);
+        PublicKey pubUser = keypairUser.getPublic();
+
+        CertificateIssuerName issuernameObj = new CertificateIssuerName(new X500Name(issuerDN));
+
+        certInfo = createX509CertInfo(convertPublicKeyToX509Key(pubUser),
+                                      BigInteger.valueOf(1),issuernameObj,subjectDN,
+                                      notBefore, notAfter, "SHA256withRSA");
+
+        certImpl = new X509CertImpl(certInfo);
+        certImpl.sign(keypairCA.getPrivate(),"SHA256withRSA");
+
+        String certOutput = certImpl.toString();
+
+        System.out.println("Test certificate output: \n" + certOutput);
+    }
+
+    public static void testKeys(KeyPair keypair) throws Exception {
+        PublicKey pubKey = keypair.getPublic();
+        PrivateKey privKey = keypair.getPrivate();
+
+        if (pubKey instanceof ECPublicKey) {
+            assert(privKey instanceof ECPrivateKey);
+
+            ECPublicKey ecPub = (ECPublicKey)pubKey;
+            ECPrivateKey ecPriv = (ECPrivateKey)privKey;
+            // Two public functions we want to test on ECPublicKey:
+            // 1. getW() -> ECPoint
+            // 2. getParams() -> ECParameterSpec
+
+            assert(ecPub.getW() != null);
+            assert(ecPub.getW().getAffineX() != null);
+            assert(ecPub.getW().getAffineY() != null);
+
+            assert(ecPub.getParams() != null);
+            assert(ecPriv.getParams() != null);
+
+            // Since ECParameterSpec doesn't define a comparator, we have to
+            // check each member individually.
+            assert(ecPub.getParams().getCofactor() == ecPriv.getParams().getCofactor());
+            assert(ecPub.getParams().getCurve().equals(ecPriv.getParams().getCurve()));
+            assert(ecPub.getParams().getGenerator().getAffineX().equals(ecPriv.getParams().getGenerator().getAffineX()));
+            assert(ecPub.getParams().getGenerator().getAffineY().equals(ecPriv.getParams().getGenerator().getAffineY()));
+            assert(ecPub.getParams().getOrder().equals(ecPriv.getParams().getOrder()));
+        }
+    }
+
+    public static X509CertInfo createX509CertInfo(X509Key x509key, BigInteger serialno,
+                                                  CertificateIssuerName issuernameObj,
+                                                  String subjname, Date notBefore,
+                                                  Date notAfter, String alg)
+            throws Exception
+    {
         X509CertInfo info = new X509CertInfo();
 
         info.set(X509CertInfo.VERSION, new
@@ -126,8 +190,7 @@ public class X509CertTest {
         return info;
     }
 
-    public static X509Key convertPublicKeyToX509Key(PublicKey pubk)
-            throws InvalidKeyException , IOException {
+    public static X509Key convertPublicKeyToX509Key(PublicKey pubk) throws Exception {
         X509Key xKey = null;
 
         if (pubk instanceof RSAPublicKey) {
