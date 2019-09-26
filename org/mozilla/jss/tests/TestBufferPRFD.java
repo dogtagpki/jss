716a746436c139219215aaefa8fb90cca35bb51c
Alexander Scheel <ascheel@redhat.com>
2019-09-26 10:19:53 -0400

    Remove excessive import
    Under the javac shipped in RHEL 7, we see the following error when
    building the test suite:
    
    jss/org/mozilla/jss/tests/TestBufferPRFD.java:8: error: package org.mozilla.jss.pkix does not exist
    import org.mozilla.jss.pkix.*;
    ^
    
    This is because the org.mozilla.jss.pkix namespace doesn't include any
    classes; it only contains sub-packages.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/tests/TestBufferPRFD.java
+++ org/mozilla/jss/tests/TestBufferPRFD.java
@@ -5,7 +5,6 @@ import java.util.*;
 import org.mozilla.jss.*;
 import org.mozilla.jss.crypto.*;
 import org.mozilla.jss.pkcs11.*;
-import org.mozilla.jss.pkix.*;
 import org.mozilla.jss.nss.*;
 import org.mozilla.jss.ssl.*;
 import org.mozilla.jss.util.*;
