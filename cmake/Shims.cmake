12a85a262a763bf6828432e8dc00ad38cd7777db
Alexander Scheel <ascheel@redhat.com>
2019-10-01 10:53:56 -0400

    Fix build on older CMake versions
    On CMake versions less than 3.12.0 (shipped July 17, 2018), the
    list(...) helper lacks the JOIN subcommand. We use this for joining the
    C compiler flags into CMAKE_REQUIRED_FLAGS for symbol detection.
    
    This introduces a shim for older systems lacking these useful macros,
    including RHEL 8.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ cmake/Shims.cmake
@@ -0,0 +1,15 @@
+# LIST(JOIN ...) was introduced in CMake verison 3.12
+macro(jss_list_join LIST_ SEPARATOR_ VAR_)
+    if(${CMAKE_VERSION} VERSION_LESS "3.12.0")
+        set("${VAR_}" "")
+        foreach(ELEMENT ${${LIST_}})
+            message(STATUS "ELEM: ${ELEMENT}")
+            set("${VAR_}" "${${VAR_}}${SEPARATOR_}${ELEMENT}")
+        endforeach()
+        string(LENGTH "${SEPARATOR_}" JSS_LIST_JOIN_SEPARATOR_LENGTH)
+        string(SUBSTRING "${${VAR_}}" ${JSS_LIST_JOIN_SEPARATOR_LENGTH} -1 "${VAR_}")
+    else()
+        list(JOIN ${LIST_} ${SEPARATOR_} ${VAR_})
+    endif()
+endmacro()
+
