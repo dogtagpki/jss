0604791d7dd526773e6f9e296221b320333b1e35
Alexander Scheel <ascheel@redhat.com>
2020-01-14 16:22:42 -0500

    Add toString to SecurityStatusResult
    This aids logging in the JSSEngine.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/SecurityStatusResult.java
+++ org/mozilla/jss/nss/SecurityStatusResult.java
@@ -1,5 +1,7 @@
 package org.mozilla.jss.nss;
 
+import java.lang.StringBuilder;
+
 /**
  * The fields in a SecurityStatusResult indicate whether a given SSL-enabled
  * PRFileDesc has completed its handshake and the resulting handshake-specific
@@ -35,6 +37,26 @@ public class SecurityStatusResult {
         this.secretKeySize = _secretKeySize;
         this.issuer = _issuer;
         this.subject = _subject;
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder("SecurityStatusResult:");
+        result.append("\n- on: " + on);
+        if (cipher != null && cipher.length > 0) {
+            result.append("\n- cipher: ");
+            result.append(new String(cipher));
+        }
+        result.append("\n- keySize: " + keySize);
+        result.append("\n- secretKeySize: " + secretKeySize);
+        if (issuer != null && issuer.length > 0) {
+            result.append("\n- issuer: ");
+            result.append(new String(issuer));
+        }
+        if (subject != null && subject.length > 0) {
+            result.append("\n- subject: ");
+            result.append(new String(subject));
+        }
+        return result.toString();
     }
 }
 
