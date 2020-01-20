7cc34710dc43f509e74536db7582d4b62a5c0bbe
Alexander Scheel <ascheel@redhat.com>
2020-01-20 13:02:09 -0500

    Add SOCKET_SHUTDOWN_ERROR to PRErrors
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/PRErrors.c
+++ org/mozilla/jss/nss/PRErrors.c
@@ -15,3 +15,9 @@ Java_org_mozilla_jss_nss_PRErrors_getWouldBlockError(JNIEnv *env, jclass clazz)
 {
     return PR_WOULD_BLOCK_ERROR;
 }
+
+JNIEXPORT int JNICALL
+Java_org_mozilla_jss_nss_PRErrors_getSocketShutdownError(JNIEnv *env, jclass clazz)
+{
+    return PR_SOCKET_SHUTDOWN_ERROR;
+}
