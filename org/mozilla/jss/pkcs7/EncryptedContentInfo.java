dogtagpki/jss

commit 13998a9e77e60d6509ac814ed711dd21e1248ecd

Fix jss internal deprecation warnings #35

org/mozilla/jss/pkcs7/EncryptedContentInfo.java
@@ -182,12 +182,17 @@ public class EncryptedContentInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            params = new IVParameterSpec( kg.generatePBE_IV() );
-        } else if( encAlg.getParameterClass().equals(
-                        RC2ParameterSpec.class ) ) {
-            params = new RC2ParameterSpec(key.getStrength(),
-                                          kg.generatePBE_IV());
+        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals(
+                      javax.crypto.spec.IvParameterSpec.class ) ) {
+                params = new IVParameterSpec(kg.generatePBE_IV());
+                break;
+            } else if ( paramClasses[i].equals( RC2ParameterSpec.class ) ) {
+                params = new RC2ParameterSpec(key.getStrength(),
+                                              kg.generatePBE_IV());
+                break;
+            }
         }
 
         // perform encryption
@@ -276,12 +281,17 @@ public class EncryptedContentInfo implements ASN1Value {
         // compute algorithm parameters
         EncryptionAlgorithm encAlg = ((PBEAlgorithm)kgAlg).getEncryptionAlg();
         AlgorithmParameterSpec algParams = null;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            algParams = new IVParameterSpec( kg.generatePBE_IV() );
-        } else if( encAlg.getParameterClass().equals(
-                       RC2ParameterSpec.class ) ) {
-            algParams = new RC2ParameterSpec(key.getStrength(),
-                                             kg.generatePBE_IV());
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals(
+                      javax.crypto.spec.IvParameterSpec.class ) ) {
+                algParams = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            } else if ( paramClasses[i].equals(RC2ParameterSpec.class ) ) {
+                algParams = new RC2ParameterSpec(key.getStrength(),
+                                                 kg.generatePBE_IV());
+                break;
+            }
         }
 
         // perform the decryption

org/mozilla/jss/pkix/cms/EncryptedContentInfo.java
@@ -180,8 +180,12 @@ public class EncryptedContentInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            params = new IVParameterSpec( kg.generatePBE_IV() );
+        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals( IVParameterSpec.class ) ) {
+                params = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            }
         }
 
         // perform encryption
@@ -269,11 +273,14 @@ public class EncryptedContentInfo implements ASN1Value {
 
         // compute algorithm parameters
         EncryptionAlgorithm encAlg = ((PBEAlgorithm)kgAlg).getEncryptionAlg();
-        AlgorithmParameterSpec algParams;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            algParams = new IVParameterSpec( kg.generatePBE_IV() );
-        } else {
-            algParams = null;
+        AlgorithmParameterSpec algParams = null;
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals(
+                       javax.crypto.spec.IvParameterSpec.class ) ) {
+                algParams = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            }
         }
 
         // perform the decryption

org/mozilla/jss/pkix/primitive/EncryptedPrivateKeyInfo.java
@@ -147,8 +147,12 @@ public class EncryptedPrivateKeyInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            params = new IVParameterSpec( kg.generatePBE_IV() );
+        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals( javax.crypto.spec.IvParameterSpec.class ) ) {
+                params = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            }
         }
 
         // perform encryption
@@ -324,8 +328,13 @@ public class EncryptedPrivateKeyInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            params = new IVParameterSpec( kg.generatePBE_IV() );
+        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals(
+                      javax.crypto.spec.IvParameterSpec.class ) ) {
+                params = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            }
         }
 
         KeyWrapper wrapper = token.getKeyWrapper(
@@ -404,11 +413,14 @@ public class EncryptedPrivateKeyInfo implements ASN1Value {
 
         // compute algorithm parameters
         EncryptionAlgorithm encAlg = ((PBEAlgorithm)kgAlg).getEncryptionAlg();
-        AlgorithmParameterSpec algParams;
-        if( encAlg.getParameterClass().equals( IVParameterSpec.class ) ) {
-            algParams = new IVParameterSpec( kg.generatePBE_IV() );
-        } else {
-            algParams = null;
+        AlgorithmParameterSpec algParams = null;
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
+        for (int i = 0; i < paramClasses.length; i ++) {
+            if ( paramClasses[i].equals(
+                      javax.crypto.spec.IvParameterSpec.class ) ) {
+                algParams = new IVParameterSpec( kg.generatePBE_IV() );
+                break;
+            }
         }
 
         // perform the decryption

commit e2e2844eaa70679718424181b5c9fe694207df70

Fix swapped parameter names with PBE
Commit 13998a9e77e60d6509ac814ed711dd21e1248ecd introduced a regression
related to extracting the parameter classes during PBE operations:
previously, the classes of the underlying encryption algorithm were
iterated over, instead of the classes of the PBE class itself. However,
this commit iterated over the PBE parameter classes; no PBE algorithm
accepts a IvParameterSpec, resulting in a null parameter passed to the
later encryption or key wrap operation. This resulted in stack traces
like the following:

Caused by: java.security.InvalidAlgorithmParameterException: DES3/CBC/Pad cannot use a null parameter
	at org.mozilla.jss.pkcs11.PK11KeyWrapper.checkParams(PK11KeyWrapper.java:225)
	at org.mozilla.jss.pkcs11.PK11KeyWrapper.initWrap(PK11KeyWrapper.java:89)
	at org.mozilla.jss.pkcs11.PK11KeyWrapper.initWrap(PK11KeyWrapper.java:57)
	at org.mozilla.jss.pkix.primitive.EncryptedPrivateKeyInfo.createPBE(EncryptedPrivateKeyInfo.java:342)

Resolves: rh-bz#1807371

Signed-off-by: Alexander Scheel <ascheel@redhat.com>

org/mozilla/jss/pkcs7/EncryptedContentInfo.java
@@ -182,7 +182,7 @@ public class EncryptedContentInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
         for (int i = 0; i < paramClasses.length; i ++) {
             if ( paramClasses[i].equals(
                       javax.crypto.spec.IvParameterSpec.class ) ) {

org/mozilla/jss/pkix/cms/EncryptedContentInfo.java
@@ -180,7 +180,7 @@ public class EncryptedContentInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
         for (int i = 0; i < paramClasses.length; i ++) {
             if ( paramClasses[i].equals( IVParameterSpec.class ) ) {
                 params = new IVParameterSpec( kg.generatePBE_IV() );

org/mozilla/jss/pkix/primitive/EncryptedPrivateKeyInfo.java
@@ -147,7 +147,7 @@ public class EncryptedPrivateKeyInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
         for (int i = 0; i < paramClasses.length; i ++) {
             if ( paramClasses[i].equals( javax.crypto.spec.IvParameterSpec.class ) ) {
                 params = new IVParameterSpec( kg.generatePBE_IV() );
@@ -328,7 +328,7 @@ public class EncryptedPrivateKeyInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
         for (int i = 0; i < paramClasses.length; i ++) {
             if ( paramClasses[i].equals(
                       javax.crypto.spec.IvParameterSpec.class ) ) {
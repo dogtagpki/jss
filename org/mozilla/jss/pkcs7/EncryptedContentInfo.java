e2e2844eaa70679718424181b5c9fe694207df70
Alexander Scheel <ascheel@redhat.com>
2020-02-28 14:10:32 -0500

    Fix swapped parameter names with PBE
    Commit 13998a9e77e60d6509ac814ed711dd21e1248ecd introduced a regression
    related to extracting the parameter classes during PBE operations:
    previously, the classes of the underlying encryption algorithm were
    iterated over, instead of the classes of the PBE class itself. However,
    this commit iterated over the PBE parameter classes; no PBE algorithm
    accepts a IvParameterSpec, resulting in a null parameter passed to the
    later encryption or key wrap operation. This resulted in stack traces
    like the following:
    
    Caused by: java.security.InvalidAlgorithmParameterException: DES3/CBC/Pad cannot use a null parameter
    	at org.mozilla.jss.pkcs11.PK11KeyWrapper.checkParams(PK11KeyWrapper.java:225)
    	at org.mozilla.jss.pkcs11.PK11KeyWrapper.initWrap(PK11KeyWrapper.java:89)
    	at org.mozilla.jss.pkcs11.PK11KeyWrapper.initWrap(PK11KeyWrapper.java:57)
    	at org.mozilla.jss.pkix.primitive.EncryptedPrivateKeyInfo.createPBE(EncryptedPrivateKeyInfo.java:342)
    
    Resolves: rh-bz#1807371
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/pkcs7/EncryptedContentInfo.java
+++ org/mozilla/jss/pkcs7/EncryptedContentInfo.java
@@ -182,7 +182,7 @@ public class EncryptedContentInfo implements ASN1Value {
         // generate IV
         EncryptionAlgorithm encAlg = pbeAlg.getEncryptionAlg();
         AlgorithmParameterSpec params=null;
-        Class<?> [] paramClasses = pbeAlg.getParameterClasses();
+        Class<?> [] paramClasses = encAlg.getParameterClasses();
         for (int i = 0; i < paramClasses.length; i ++) {
             if ( paramClasses[i].equals(
                       javax.crypto.spec.IvParameterSpec.class ) ) {
