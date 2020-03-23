3f2661f48f15b8868903f9402c9a1efb3f638e3a
Alexander Scheel <ascheel@redhat.com>
2020-03-23 14:46:53 -0400

    Add SSL_InvalidateSession
    This allows the caller to invalidate the current session on a SSLFDProxy
    instance.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/nss/SSL.c
+++ org/mozilla/jss/nss/SSL.c
@@ -678,6 +678,22 @@ Java_org_mozilla_jss_nss_SSL_ConfigServerSessionIDCache(JNIEnv *env, jclass claz
     return ret;
 }
 
+JNIEXPORT int JNICALL
+Java_org_mozilla_jss_nss_SSL_InvalidateSession(JNIEnv *env, jclass clazz,
+    jobject fd)
+{
+    PRFileDesc *real_fd = NULL;
+
+    PR_ASSERT(env != NULL && fd != NULL);
+    PR_SetError(0, 0);
+
+    if (JSS_PR_getPRFileDesc(env, fd, &real_fd) != PR_SUCCESS) {
+        return SECFailure;
+    }
+
+    return SSL_InvalidateSession(real_fd);
+}
+
 JNIEXPORT jobject JNICALL
 Java_org_mozilla_jss_nss_SSL_PeerCertificate(JNIEnv *env, jclass clazz,
     jobject fd)
