97e5597f90df84e59e2c5669d49fed73b4681600
Alexander Scheel <ascheel@redhat.com>
2020-03-30 17:26:04 -0400

    Migrate tests to local CryptoManager configuration
    Two tests require the new local-preferential CryptoManager
    initialization: SetupDBs and FipsTest. Refactor these tests.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/tests/SetupDBs.java
+++ org/mozilla/jss/tests/SetupDBs.java
@@ -4,7 +4,7 @@
 
 package org.mozilla.jss.tests;
 
-import org.mozilla.jss.CryptoManager;
+import org.mozilla.jss.*;
 import org.mozilla.jss.crypto.*;
 import org.mozilla.jss.util.*;
 
@@ -15,9 +15,8 @@ import org.mozilla.jss.util.*;
 
 public class SetupDBs {
 
-    public static void main(String args[]) {
-      try {
-        if( args.length != 2 ) {
+    public static void main(String args[]) throws Exception {
+        if (args.length != 2) {
             System.err.println("Usage: java org.mozilla.jss.tests.SetupDBs " +
 			       "<dbdir> <passwordFile>\n" + 
                                "Password file should have format:\n " +
@@ -26,21 +25,20 @@ public class SetupDBs {
                                "Key=m1oZilla\n");
             System.exit(1);
         }
-        String dbdir = args[0];
-        
-        CryptoManager.initialize(dbdir);
+
+        // Initialize JSS, preferring the local CryptoManager initialization
+        // over the one from java.security.
+        InitializationValues ivs = new InitializationValues(args[0]);
+        CryptoManager.initialize(ivs);
         CryptoManager cm = CryptoManager.getInstance();
 
+        // Get the internal key storage token so we can set the password.
         CryptoToken tok = cm.getInternalKeyStorageToken();
-        tok.initPassword( new NullPasswordCallback(),
-            new FilePasswordCallback( args[1] )
-        );
-        
-        System.exit(0);
-      } catch(Exception e) {
-        e.printStackTrace();
-        System.exit(1);
-      }
-    }
 
+        // Set the user password to the one from the password file; the
+        // security officer password is empty.
+        PasswordCallback securityOfficerPassword = new NullPasswordCallback();
+        PasswordCallback userPassword = new FilePasswordCallback(args[1]);
+        tok.initPassword(securityOfficerPassword, userPassword);
+    }
 }
