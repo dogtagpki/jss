0450bc685a5c24cc719615d01bd468b0e61c7781
Alexander Scheel <ascheel@redhat.com>
2020-05-22 09:59:49 -0400

    Split JSSKeyManager interface, implementation
    JSSKeyManager extends X509KeyManager and provides one additional call
    that is used by JSSEngine. We also introduce a token-backed KeyManager,
    using the existing KeyStore API. When no KeyStore is provided, this
    KeyManager falls back to loading certificates from the CryptoManager,
    matching existing behavior.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/provider/javax/crypto/JSSKeyManagerFactory.java
+++ org/mozilla/jss/provider/javax/crypto/JSSKeyManagerFactory.java
@@ -8,7 +8,26 @@ import org.mozilla.jss.util.*;
 import org.mozilla.jss.provider.java.security.*;
 
 public class JSSKeyManagerFactory extends KeyManagerFactorySpi {
+    private KeyStore internalStorage;
+    char[] password;
+
     protected void engineInit(KeyStore ks, char[] password) throws KeyStoreException {
+        if (password != null) {
+            engineInitCryptoManager(password);
+
+            // We need to keep this password around and give it to the
+            // JSSKeyManager instance; the KeyStore API requires a password
+            // to open private keys, though JSS might not necessarily use
+            // that.
+            this.password = password;
+        }
+
+        if (ks != null) {
+            engineInitKeyStore(ks);
+        }
+    }
+
+    protected void engineInitCryptoManager(char[] password) throws KeyStoreException {
         try {
             // At most, we can update the password callback if one doesn't exist.
             CryptoManager cm = CryptoManager.getInstance();
@@ -18,18 +37,33 @@ public class JSSKeyManagerFactory extends KeyManagerFactorySpi {
                 cm.setPasswordCallback(callback);
             }
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
+            throw new KeyStoreException(e.getMessage(), e);
+        }
+    }
+
+    protected void engineInitKeyStore(KeyStore ks) throws KeyStoreException {
+        try {
+            if (!ks.getProvider().getName().equals("Mozilla-JSS")) {
+                String msg = "Unable to initialize JSSKeyManagerFactory ";
+                msg += "with key store from non-JSS provider.";
+                throw new KeyStoreException(msg);
+            }
+
+            internalStorage = ks;
+        } catch (Exception e) {
+            throw new KeyStoreException(e.getMessage(), e);
         }
     }
 
     protected void engineInit(ManagerFactoryParameters spec) {
-        // There is absolutely nothing we can do here, so exit without
-        // initializing anything.
+        // There is nothing we can do here, so exit without initializing
+        // anything. In the future, we can provide a method to return
+        // multiple token-specific KeyManagers.
     }
 
     protected KeyManager[] engineGetKeyManagers() {
         KeyManager[] kms = new KeyManager[1];
-        kms[0] = new JSSKeyManager();
+        kms[0] = new JSSTokenKeyManager(internalStorage, password);
 
         return kms;
     }
---
+++ org/mozilla/jss/provider/javax/crypto/JSSTokenKeyManager.java
@@ -0,0 +1,136 @@
+/* BEGIN COPYRIGHT BLOCK
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ * All rights reserved.
+ * END COPYRIGHT BLOCK */
+
+package org.mozilla.jss.provider.javax.crypto;
+
+import java.net.Socket;
+import java.security.KeyStore;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.net.ssl.X509KeyManager;
+
+import org.mozilla.jss.CryptoManager;
+import org.mozilla.jss.NotInitializedException;
+import org.mozilla.jss.crypto.ObjectNotFoundException;
+import org.mozilla.jss.pkcs11.PK11Cert;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JSSTokenKeyManager implements JSSKeyManager {
+
+    final static Logger logger = LoggerFactory.getLogger(JSSTokenKeyManager.class);
+
+    private KeyStore jks;
+    private CryptoManager cm;
+    private char[] password;
+
+    public JSSTokenKeyManager(KeyStore jssKeyStore, char[] password) {
+        jks = jssKeyStore;
+        this.password = password;
+
+        try {
+            cm = CryptoManager.getInstance();
+        } catch (NotInitializedException nie) {
+            String msg = "CryptoManager reported as not initialized but have ";
+            msg += "a working KeyStore instance! " + nie.getMessage();
+            throw new RuntimeException(msg, nie);
+        }
+    }
+
+    @Override
+    public String chooseClientAlias(String[] keyTypes, Principal[] issuers, Socket socket) {
+        logger.debug("JSSKeyManager: chooseClientAlias() - not implemented");
+        return null;
+    }
+
+    @Override
+    public String chooseServerAlias(String keyType, Principal[] issuers, Socket socket) {
+        logger.debug("JSSKeyManager: chooseServerAlias() - not implemented");
+        return null;
+    }
+
+    public org.mozilla.jss.crypto.X509Certificate getCertificate(String alias) {
+        try {
+            if (jks == null) {
+                return cm.findCertByNickname(alias);
+            }
+
+            return (org.mozilla.jss.crypto.X509Certificate) jks.getCertificate(alias);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public X509Certificate[] getCertificateChain(String alias) {
+        logger.debug("JSSKeyManager: getCertificateChain(" + alias + ")");
+
+        try {
+            org.mozilla.jss.crypto.X509Certificate cert = getCertificate(alias);
+            org.mozilla.jss.crypto.X509Certificate[] chain = cm.buildCertificateChain(cert);
+
+            logger.debug("JSSKeyManager: cert chain:");
+
+            Collection<org.mozilla.jss.pkcs11.PK11Cert> list = new ArrayList<>();
+            for (org.mozilla.jss.crypto.X509Certificate c : chain) {
+                logger.debug("JSSKeyManager: - " + c.getSubjectDN());
+                list.add((PK11Cert) c);
+            }
+
+            return list.toArray(new X509Certificate[list.size()]);
+        } catch (Throwable e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String[] getClientAliases(String keyType, Principal[] issuers) {
+        logger.debug("JSSKeyManager: getClientAliases() - not implemented");
+        return null;
+    }
+
+    @Override
+    public PrivateKey getPrivateKey(String alias) {
+
+        logger.debug("JSSKeyManager: getPrivateKey(" + alias + ")");
+
+        try {
+            if (jks == null) {
+                org.mozilla.jss.crypto.X509Certificate cert = cm.findCertByNickname(alias);
+                return cm.findPrivKeyByCert(cert);
+            }
+
+            return (PrivateKey) jks.getKey(alias, password);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public String[] getServerAliases(String keyType, Principal[] issuers) {
+        logger.debug("JSSKeyManager: getServerAliases() - not implemented");
+        return null;
+    }
+}
