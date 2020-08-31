20765fc773b461a1c106a9fe2028c30c771d584c
Alexander Scheel <ascheel@redhat.com>
2020-08-31 14:48:02 -0400

    Add SSLErrors with SSL_ERROR_BAD_CERT_DOMAIN
    This is the only SSLError we wish to allow through in regular JSSEngine
    client operations.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ org/mozilla/jss/nss/SSLErrors.c
@@ -0,0 +1,20 @@
+#include <nspr.h>
+#include <limits.h>
+#include <stdint.h>
+#include <jni.h>
+#include <nss.h>
+#include <ssl.h>
+#include <sslerr.h>
+
+#include "jssutil.h"
+#include "PRFDProxy.h"
+#include "BufferProxy.h"
+#include "BufferPRFD.h"
+
+#include "_jni/org_mozilla_jss_nss_SECErrors.h"
+
+JNIEXPORT int JNICALL
+Java_org_mozilla_jss_nss_SECErrors_getBadCertDomain(JNIEnv *env, jclass clazz)
+{
+    return SSL_ERROR_BAD_CERT_DOMAIN;
+}
