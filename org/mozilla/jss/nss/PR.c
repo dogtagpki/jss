dogtagpki/jss

commit 424d55639c51bca18110dcff51fb5df2e2e35a41

Workaround NSS PR_Read bug
When using SSL on certain systems, NSS will indicate it only has a
single byte of data to read, when in fact it has more data than that.
This is due to a bug in the TLS record parsing logic, forcing the caller
to issue another PR_Read call.

This hangs up our SSLEngine implementation. Bypass this bug by detecting
if the underlying socket is non-blocking, and if so, call PR_Read until
we get a PR_WOULD_BLOCK_ERROR.

Signed-off-by: Alexander Scheel <ascheel@redhat.com>

org/mozilla/jss/nss/PR.c
@@ -140,7 +140,10 @@ Java_org_mozilla_jss_nss_PR_Read(JNIEnv *env, jclass clazz, jobject fd,
     PRFileDesc *real_fd = NULL;
     jobject result = NULL;
     int read_amount = 0;
+    int this_read = 0;
     uint8_t *buffer = NULL;
+    PRSocketOptionData opt = { 0 };
+    PRDescType fd_type;
 
     PR_ASSERT(env != NULL && fd != NULL && amount >= 0);
     PR_SetError(0, 0);
@@ -149,14 +152,48 @@ Java_org_mozilla_jss_nss_PR_Read(JNIEnv *env, jclass clazz, jobject fd,
         return NULL;
     }
 
+    fd_type = PR_GetDescType(real_fd);
+    opt.value.non_blocking = PR_FALSE;
+
+    if (fd_type == PR_DESC_SOCKET_TCP ||
+            fd_type == PR_DESC_SOCKET_UDP ||
+            fd_type == PR_DESC_LAYERED) {
+        opt.option = PR_SockOpt_Nonblocking;
+        if (PR_GetSocketOption(real_fd, &opt) != PR_SUCCESS) {
+            /* Unable to get the value of non_blocking status; so error on
+             * the side of caution. */
+            opt.value.non_blocking = PR_FALSE;
+        }
+    }
+
     PR_ASSERT(real_fd != NULL);
 
     buffer = calloc(amount, sizeof(uint8_t));
 
-    read_amount = PR_Read(real_fd, buffer, amount);
+    /* Work around a bug in NSS/NSPR: sometimes PR_Read returns a much smaller
+     * read than expected, when it could read much more. */
+    while (read_amount < amount) {
+        this_read = PR_Read(real_fd, buffer + read_amount, amount - read_amount);
+        if (this_read <= 0) {
+            if (PR_GetError() == PR_WOULD_BLOCK_ERROR && read_amount > 0) {
+                /* If we've previously gotten data and we would block this
+                 * time, then we're at the end of our data. Reset the error
+                 * status and break, rather than exiting with an error. */
+                PR_SetError(0, 0);
+                break;
+            }
 
-    if (read_amount <= 0) {
-        goto done;
+            goto done;
+        } else {
+            read_amount += this_read;
+
+            if (opt.value.non_blocking != PR_TRUE) {
+                /* When we're not non-blocking, it isn't necessarily safe to
+                 * call PR_Read again -- a call that would've not blocked will
+                 * now block. */
+                break;
+            }
+        }
     }
 
     result = JSS_ToByteArray(env, buffer, read_amount);

commit 905718f2a8fb441825b51fae0f2481965456a160

PR.Read: return data when end of stream reached
Commit 424d55639c51bca18110dcff51fb5df2e2e35a41 introduced a
workaround in PR.Read for a bug in NSS' PR_Read that occurs on some
platforms.  The bug can cause PR_Read to return a smaller amount
than requested, even when more data is available.  The workaround is
to loop until we have either read the requested amount, or we
encounter PR_WOULD_BLOCK_ERROR.

Unfortunatley there is a bug in the workaround: the end of stream
case was not considered.  In this case, the first PR_Read reads less
than the requested amount, and we loop.  The next PR_Read returns 0,
but the "not an error" condition

    PR_GetError() == PR_WOULD_BLOCK_ERROR && read_amount > 0

does not admit the end of stream case (PR_GetError() == 0).  As a
consequence, PR.Read returns an empty byte[] and the data from the
final read before end of stream are lost.

To resolve this issue, break the loop when PR_Read returns <= 0 but
PR_GetError() returns 0.

org/mozilla/jss/nss/PR.c
@@ -176,6 +176,10 @@ Java_org_mozilla_jss_nss_PR_Read(JNIEnv *env, jclass clazz, jobject fd,
     while (read_amount < amount) {
         this_read = PR_Read(real_fd, buffer + read_amount, amount - read_amount);
         if (this_read <= 0) {
+            if (PR_GetError() == 0) {
+                /* End of data */
+                break;
+            }
             if (PR_GetError() == PR_WOULD_BLOCK_ERROR && read_amount > 0) {
                 /* If we've previously gotten data and we would block this
                  * time, then we're at the end of our data. Reset the error