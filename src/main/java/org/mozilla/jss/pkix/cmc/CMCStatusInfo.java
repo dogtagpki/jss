d84855b2fae7c58fc2a8fe69b2c33a23dfaebf45
Chris Kelley <ckelley@redhat.com>
2021-05-21 10:46:10 +0100

    Add missing annotations to org.mozilla.jss.pkix.cmc

--- src/main/java/org/mozilla/jss/pkix/cmc/CMCStatusInfo.java
+++ src/main/java/org/mozilla/jss/pkix/cmc/CMCStatusInfo.java
@@ -158,14 +158,17 @@ public class CMCStatusInfo implements ASN1Value {
     ///////////////////////////////////////////////////////////////////////
 
     public static final Tag TAG = SEQUENCE.TAG;
+    @Override
     public Tag getTag() {
         return TAG;
     }
 
+    @Override
     public void encode(OutputStream ostream) throws IOException {
         encode(TAG, ostream);
     }
 
+    @Override
     public void encode(Tag implicitTag, OutputStream ostream)
         throws IOException
     {
@@ -203,15 +206,18 @@ public class CMCStatusInfo implements ASN1Value {
             seqt.addOptionalElement( OtherInfo.getTemplate() );
         }
 
+        @Override
         public boolean tagMatch(Tag tag) {
             return TAG.equals(tag);
         }
 
+        @Override
         public ASN1Value decode(InputStream istream)
                 throws InvalidBERException, IOException {
             return decode(TAG, istream);
         }
 
+        @Override
         public ASN1Value decode(Tag implicitTag, InputStream istream)
                 throws InvalidBERException, IOException {
 
