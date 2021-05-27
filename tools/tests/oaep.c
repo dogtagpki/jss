ae0462a94437e9bc44c838e8995bdb1658eb06db
Chris Kelley <ckelley@redhat.com>
2021-05-27 11:05:43 +0100

    Autoformat tools

--- tools/tests/oaep.c
+++ tools/tests/oaep.c
@@ -4,7 +4,7 @@
 #include "pk11pub.h"
 
 int main() {
-    PK11_PubWrapSymKeyWithMechanism(NULL, 0, NULL, NULL, NULL);
-    PK11_PubUnwrapSymKeyWithMechanism(NULL, 0, NULL, NULL, 0, 0, 0);
-    return 0;
+	PK11_PubWrapSymKeyWithMechanism(NULL, 0, NULL, NULL, NULL);
+	PK11_PubUnwrapSymKeyWithMechanism(NULL, 0, NULL, NULL, 0, 0, 0);
+	return 0;
 }
