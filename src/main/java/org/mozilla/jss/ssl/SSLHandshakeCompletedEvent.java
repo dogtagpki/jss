0cf91e9cf00e70acd902a7401af93939f373066d
Alexander Scheel <alexander.m.scheel@gmail.com>
2021-09-08 14:04:10 -0400

    Update SSLHandshakeCompletedEvent to support firing on SSLEngine instances
    SSLHandshakeCompletedEvent lacked support and knowledge of SSLEngine,
    and instead only supports the old org.mozilla.jss.ssl.SSLSocket class.
    Eventually support for org.mozilla.jss.ssl.javax.SSLSocket should also
    be added.
    
    At this point in time, the conversion to
    javax.net.ssl.HandshakeCompletedEvent has not been done. In particular,
    this event is only useful to SSLSockets (and wouldn't work with our
    old org.mozilla.jss.ssl.SSLSocket class as it only implements
    javax.net.Socket).
    
    While this event is less useful from a SSLEngine Point of View (as a
    SSLEngine directly returns FINISHED as a handshake status after a call
    to wrap/unwrap at some point in time), note that it is still useful for
    when the caller has control over adding listeners into the SSLEngine but
    sometimes lacks access to the result of wrap/unwrap -- such as in a
    Apache Tomcat or Apache http components library environment. Here,
    default JSS SSLContext JSSParameters could potentially be set with
    desired listeners and then all future engines could be constructed with
    these listeners.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- src/main/java/org/mozilla/jss/ssl/SSLHandshakeCompletedEvent.java
+++ src/main/java/org/mozilla/jss/ssl/SSLHandshakeCompletedEvent.java
@@ -12,6 +12,8 @@ package org.mozilla.jss.ssl;
 import java.net.*;
 import java.util.*;
 
+import org.mozilla.jss.ssl.javax.JSSEngine;
+
 /*
  * right now, this only extends EventObject, but it will eventually
  * extend javax.net.ssl.HandshakeCompletedEvent
@@ -25,21 +27,44 @@ public class SSLHandshakeCompletedEvent extends EventObject {
     private static final long serialVersionUID = 1L;
 
     public SSLHandshakeCompletedEvent(SSLSocket socket) {
-	super(socket);
+        super(socket);
+    }
+
+    public SSLHandshakeCompletedEvent(JSSEngine engine) {
+        super(engine);
     }
     
     /**
-     * get security information about this socket, including
-     * cert data
+     * Get security information about this socket, including
+     * cert data; null if on a SSLEngine.
      */
     public SSLSecurityStatus getStatus() throws SocketException {
-	return getSocket().getStatus();
+        if (getSocket() != null) {
+            return getSocket().getStatus();
+        }
+
+        return null;
     }
     
     /**
-     * get socket on which the event occured
+     * Get socket on which the event occurred; null if on a SSLEngine.
      */
     public SSLSocket getSocket() {
-	return (SSLSocket)getSource();
+        if (getSource() instanceof SSLSocket) {
+            return (SSLSocket)getSource();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get engine on which the event occurred; null if on a SSLSocket.
+     */
+    public JSSEngine getEngine() {
+        if (getSource() instanceof JSSEngine) {
+            return (JSSEngine)getSource();
+        }
+
+        return null;
     }
 }
