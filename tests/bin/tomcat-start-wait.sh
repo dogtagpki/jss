e09731685a5a831b7d39e72210b5de35fbab07c7
Chris Kelley <ckelley@redhat.com>
2022-07-25 17:46:55 +0100

    Fix test failure introduced by efforts to fix shellcheck warnings

--- tests/bin/tomcat-start-wait.sh
+++ tests/bin/tomcat-start-wait.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 NAME=$1
 URL=$2
 
@@ -8,32 +7,30 @@ then
     echo "Usage: tomcat-start-wait.sh <name> <URL>"
     exit 1
 fi
-
 if [ "$MAX_WAIT" == "" ]
 then
     MAX_WAIT=60 # seconds
 fi
-
 start_time=$(date +%s)
-
 while :
 do
     sleep 1
 
-    if [ ! "$(docker exec "$NAME" curl -IkSs "$URL")" ]
+    docker exec "$NAME" curl -IkSs "$URL"
+
+    if [ $? -eq 0 ]
     then
         break
     fi
 
     current_time=$(date +%s)
-    elapsed_time=$(("$current_time" - "$start_time"))
+    elapsed_time=$(expr "$current_time" - "$start_time")
 
     if [ "$elapsed_time" -ge "$MAX_WAIT" ]
     then
         echo "Tomcat did not start after ${MAX_WAIT}s"
         exit 1
     fi
-
     echo "Waiting for Tomcat to start (${elapsed_time}s)"
 done
 
